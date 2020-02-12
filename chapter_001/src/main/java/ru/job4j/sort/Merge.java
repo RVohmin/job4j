2d4e310ac9ad2097b0361d9855ad4c97da329dbc
RVohmin <vohmin77@gmail.com>
2020-02-12 20:34:20 +0300

    Объединить два массива [#124482]

--- chapter_001/src/main/java/ru/job4j/sort/Merge.java
+++ chapter_001/src/main/java/ru/job4j/sort/Merge.java
@@ -3,38 +3,32 @@ package ru.job4j.sort;
 import java.util.Arrays;
 
 public class Merge {
+    @SuppressWarnings("checkstyle:RightCurly")
     public int[] merge(int[] left, int[] right) {
         int[] rsl = new int[left.length + right.length];
-        int i = 0; //счетчик для исходных массивов
-        int k = 0; // счетчик для rsl
-        while (k < rsl.length) {
-            if (i > right.length - 1) {
-                rsl[k] = left[i];
-                k++;
-                continue;
-            } else if (i > left.length - 1) {
-                rsl[k] = right[i];
-                k++;
-                continue;
-            }
-            if (left[i] < right[i]) {
-                rsl[k] = left[i];
-                rsl[k + 1] = right[i];
-            } else {
-                rsl[k] = right[i];
-                rsl[k + 1] = left[i];
-            }
-            k += 2;
+    for (int k = 0, i = 0, j = 0; k < rsl.length; k++) { // i - для left, j для right
+        if (i > left.length - 1) {
+            rsl[k] = right[j];
+            j++;
+        } else if (j > right.length - 1) {
+            rsl[k] = left[i];
             i++;
+        } else if (left[i] < right[j]) {
+            rsl[k] = left[i];
+            i++;
+        } else {
+            rsl[k] = right[j];
+            j++;
         }
+    }
         return rsl;
     }
 
     public static void main(String[] args) {
         Merge process = new Merge();
         int[] rsl = process.merge(
-                new int[] {1, 3, 5},
-                new int[] {2, 4, 6, 7}
+                new int[] {1, 1},
+                new int[] {3, 4, 5}
         );
         System.out.println(Arrays.toString(rsl));
     }

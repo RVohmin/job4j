45eff867c6c9040ef578874b5c107f257e2c4d51
RVohmin <vohmin77@gmail.com>
2020-02-13 00:17:43 +0300

    1.1. Создание объекта[#235098]

--- chapter_002/src/main/java/ru/job4j/oop/Dog.java
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -4,7 +4,9 @@ package ru.job4j.oop;
  * Dog.
  */
 public class Dog {
-    Dog polkan = new Dog();
-    Dog sharik = new Dog();
-    Dog zhychka = new Dog();
+    public static void main (String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
 }

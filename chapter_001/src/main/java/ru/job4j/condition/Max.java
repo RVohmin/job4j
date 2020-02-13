c217301f0400b4818b9037421e2a437c67690715
RVohmin <vohmin77@gmail.com>
2020-02-13 20:45:00 +0300

    3. Перегрузить метод max для трех чисел[#235097]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,9 +1,18 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left;
-        result = left > right ? left : right;
-        return result;
+    public static int max(int first, int second) {
+        return first > second ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        int left = max(first, second);
+        return left > third ? left : third;
+    }
+
+    public static int max(int first, int second, int third, int forth) {
+        int left = max(first, second);
+        int right = max(third, forth);
+        return left > right ? left : right;
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,8 +6,18 @@ import static org.junit.Assert.assertThat;
 
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
+    public void when2NumbersMax1To2Then2() {
         int result = Max.max(1, 2);
         assertThat(result, is(2));
+    }
+    @Test
+    public void when3NumbersMax1To5To3Then5() {
+        int result = Max.max(1, 5, 3);
+        assertThat(result, is(5));
+    }
+    @Test
+    public void when4NumbersMax1To4To3To8Then8() {
+        int result = Max.max(1, 4, 3, 8);
+        assertThat(result, is(8));
     }
 }

f9bec76ccab1973c1078065f45f792a5e4b44e2f
RVohmin <vohmin77@gmail.com>
2020-02-17 12:21:37 +0300

    2. Scanner и чтение числа из консоли.[#235067]

--- chapter_002/src/main/java/ru/job4j/io/Matches.java
+++ chapter_002/src/main/java/ru/job4j/io/Matches.java
@@ -6,19 +6,25 @@ public class Matches {
     public static void main(String[] args) {
         int matches = 11;
         int count = 0;
+        int player;
         Scanner input = new Scanner(System.in);
-        System.out.println("Ходить начинает игрок 1 (а то мало ли) ");
         while (matches > 0) {
-            System.out.println("Возьмите спички (от 1 до 3) ");
-            int select1 = Integer.parseInt(input.nextLine());
-            matches -= select1;
-            count += 1;
-            System.out.println("На столе осталось " + matches + " спичек");
-            if (matches == 0 && count % 2 != 0) {
-                System.out.println("Игра окончена, спичек не осталось, выиграл игрок 1");
-            } else if (matches == 0) {
-                System.out.println("Игра окончена, спичек не осталось, выиграл игрок 2");
+            ++count;
+            if (count % 2 != 0) {
+                player = 1;
+            } else {
+                player = 2;
             }
+            System.out.println("Ходит игрок " + player + ". Возьмите спички (от 1 до 3): ");
+            int select = Integer.parseInt(input.nextLine());
+            System.out.println("Игрок " + player + " выбрал " + select + " спичек.");
+            matches -= select;
+            System.out.println("На столе осталось " + matches + " спичек");
+        }
+        if (matches == 0 && count % 2 != 0) {
+            System.out.println("Игра окончена, спичек не осталось, выиграл игрок 1");
+        } else if (matches == 0) {
+            System.out.println("Игра окончена, спичек не осталось, выиграл игрок 2");
         }
     }
 }

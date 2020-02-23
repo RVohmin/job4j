239a8de17ce1f86ba4c150f6955927813897149c
RVohmin <vohmin77@gmail.com>
2020-02-23 20:24:47 +0300

    2. Очередь с приоритетом на LinkedList[#235033]

--- chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -12,6 +12,9 @@ public class PriorityQueueTest {
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
+        queue.put(new Task("urgent2", 1));
+
+        System.out.println(queue);
         Task result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }

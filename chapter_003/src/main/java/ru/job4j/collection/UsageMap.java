95a7b060860b97712c3cbd5ba518f0407b8462b7
RVohmin <vohmin77@gmail.com>
2020-02-26 13:20:41 +0300

    0. Map, HashMap[#235040]

--- chapter_003/src/main/java/ru/job4j/collection/UsageMap.java
+++ chapter_003/src/main/java/ru/job4j/collection/UsageMap.java
@@ -1,6 +1,7 @@
 package ru.job4j.collection;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class UsageMap {
     public static void main(String[] args) {
@@ -8,10 +9,10 @@ public class UsageMap {
         map.put("vohmin17@gmail.com", "1. Roman Vokhmin 7");
         map.put("vohmin57@gmail.com", "2. Roman Vokhmin 5");
         map.put("vohmin37@gmail.com", "3. Roman Vokhmin 3");
-        for (String key : map.keySet()) {
-            String value = map.get(key);
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
             System.out.println("key: " + key + " value: " + value);
         }
-
     }
 }

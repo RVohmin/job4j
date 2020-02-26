c4e4cfb7af0932106040d4199ceb51e9fb66ddd3
RVohmin <vohmin77@gmail.com>
2020-02-26 08:33:33 +0300

    1. Паспорт и жители.[#235041]

---
+++ chapter_003/src/main/java/ru/job4j/collection/PassportOffice.java
@@ -0,0 +1,28 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PassportOffice {
+    private Map<String, Citizen> citizens = new HashMap<>();
+
+    public boolean add(Citizen citizen) {
+        boolean rsl = false;
+        if (!citizens.containsKey(citizen.getPassport())) {
+            citizens.put(citizen.getPassport(), citizen);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public Citizen get(String passport) {
+        Citizen value = null;
+        for (String item : citizens.keySet()) {
+            if (passport.equals(item)) {
+            value = citizens.get(item);
+            break;
+            }
+        }
+        return value;
+    }
+}
